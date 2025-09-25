00e3a46f5d1cf7912620940e893f6ef88c9ee33a
Rodrigo Fontes <rodrifontes@gmail.com>
2025-09-25 18:48:10 -0300

    feat: Inicializando o projeto, customizando as fontes e iniciando o Main

---
+++ src/Main/index.js
@@ -0,0 +1,12 @@
+import { Text } from '../components/Text';
+
+import { Container } from './styles';
+
+export default function Main() {
+  return (
+    <Container>
+      <Text color="red">Componente Main</Text>
+      <Text>Componente Main</Text>
+    </Container>
+  );
+}
