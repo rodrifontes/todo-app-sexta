de1e4a0569dce6e3a814c334d46460cf38e64c2b
Rodrigo Fontes <rodrifontes@gmail.com>
2025-10-09 21:34:49 -0300

    feat; Finalizando componetne Tasks

--- src/Main/index.js
+++ src/Main/index.js
@@ -3,12 +3,31 @@ import { Container } from './styles';
 import Header from '../components/Header';
 import Tasks from '../components/Tasks';
 
+import { tasks } from '../mocks/tasks';
+
 export default function Main() {
+  function handleDeleteTask(id) {
+    alert(`Excluir tarefa de id ${id}`);
+  }
+
+  function handleEditTask(task) {
+    alert(`Alterar tarefa de id ${task.id}`);
+  }
+
+  function handleChangeStatus(id) {
+    alert(`Excluir tarefa de id ${id}`);
+  }
+
   return (
     <Container>
       <Header />
 
-      <Tasks />
+      <Tasks
+        tasks={tasks}
+        onChangeStatus={handleChangeStatus}
+        onDelete={handleDeleteTask}
+        onEditTask={handleEditTask}
+      />
     </Container>
   );
 }
--- src/components/Tasks/styles.js
+++ src/components/Tasks/styles.js
@@ -13,4 +13,27 @@ export const TaskHeader = styled.View``;
 
 export const TaskDescription = styled.View``;
 
-export const TaskFooter = styled.View``;
+export const TaskFooter = styled.View`
+  flex-direction: row;
+  align-items: center;
+  justify-content: space-between;
+`;
+
+export const TaskIcon = styled.Image`
+  width: 16px;
+  height: 16px;
+  resize-mode: contain;
+`;
+
+export const TaskStatus = styled.TouchableOpacity`
+  flex-direction: row;
+  align-items: center;
+  gap: 8px;
+`;
+
+export const TaskActions = styled.View`
+  flex-direction: row;
+  justify-content: space-between;
+  align-items: center;
+  gap: 16px;
+`;
